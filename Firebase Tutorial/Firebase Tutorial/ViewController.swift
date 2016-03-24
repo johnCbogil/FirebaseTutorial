9a4f3644bee4572e29291b6ce1c19f00c30fd618
Bogil, John <John.Bogil@viacommix.com>
2016-03-24 15:40:14 -0400

    Started the full guide, had trouble with the security permissions

--- Firebase Tutorial/Firebase Tutorial/ViewController.swift
+++ Firebase Tutorial/Firebase Tutorial/ViewController.swift
@@ -13,20 +13,22 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
         
-        // Create a reference to a Firebase location
-        let myRootRef = Firebase(url:"https://5mintutorial.firebaseio.com")
-        // Write data to Firebase
-        myRootRef.setValue("Do you have data? You'll love Firebase123.")
+        // Get a refernce to the data
+        let ref = Firebase(url: "https://5mintutorial2.firebaseio.com/")
         
+        // Create two users as dictionaries
+        let alanisawesome = ["full_name": "Alan Turinggggg", "date_of_birth": "June 23, 1912"]
+        let gracehop = ["full_name": "Grace Hopper", "date_of_birth": "December 9, 1906"]
         
-        // Read data and react to changes
-        myRootRef.observeEventType(.Value, withBlock: {
-            snapshot in
-            print("\(snapshot.key) -> \(snapshot.value)")
-        })
+        // Get a reference to the users
+        //let usersRef = ref.childByAppendingPath("/users")
         
+        // Create a dict of users
+        let users = ["alanisawesome": alanisawesome, "gracehop": gracehop]
+        
+        // Set the users dictionary to the database of users
+        ref.setValue(users)
     }
 
     override func didReceiveMemoryWarning() {
@@ -34,6 +36,30 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    func fiveMinTutorial() {
+        // Create a reference to a Firebase location
+        let myRootRef = Firebase(url:"https://5mintutorial.firebaseio.com")
+        
+        // Write data to Firebase
+        myRootRef.setValue("Do you have data? You'll love Firebase123.")
+        
+        // Read data and react to changes
+        myRootRef.observeEventType(.Value, withBlock: {
+            snapshot in
+            print("\(snapshot.key) -> \(snapshot.value)")
+        })
+        
+        myRootRef.createUser("bobtony@example.com", password: "correcthorsebatterystaple",
+                             withValueCompletionBlock: { error, result in
+                                if error != nil {
+                                    // There was an error creating the account
+                                    print("there was an error creating the acct")
+                                } else {
+                                    let uid = result["uid"] as? String
+                                    print("Successfully created user account with uid: \(uid)")
+                                }
+        })
+    }
 
 }
 
