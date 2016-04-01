7006d1618118985fb29b8d681d3c497fdedbe635
John Bogil <johncbogil@gmail.com>
2016-04-01 17:23:08 -0400

    Added a segment control

--- Firebase Tutorial/Firebase Tutorial/ViewController.swift
+++ Firebase Tutorial/Firebase Tutorial/ViewController.swift
@@ -10,15 +10,20 @@ import UIKit
 import Firebase
 
 class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource  {
-
+    
     var uid = ""
     var userDefauts = NSUserDefaults.standardUserDefaults()
     var ref = Firebase(url: "https://5mintutorial2.firebaseio.com")
     let listOfGroups = ["ACLU", "PP", "EANY"]
+    var listOfSelectedGroups : NSMutableArray = [];
+    
+    @IBOutlet weak var segmentControl: UISegmentedControl!
     @IBOutlet weak var tableView: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        self.navigationItem.leftBarButtonItem = self.editButtonItem()
         
         self.tableView.delegate = self
         self.tableView.dataSource = self
@@ -57,22 +62,40 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
             }
         }
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
-    func tableView(tableView:UITableView, numberOfRowsInSection section:Int) -> Int
-    {
-        return self.listOfGroups.count
+    func tableView(tableView:UITableView, numberOfRowsInSection section:Int) -> Int {
+        if self.segmentControl.selectedSegmentIndex == 0 {
+            return self.listOfGroups.count
+        }
+        else {
+            return self.listOfSelectedGroups.count
+        }
     }
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         
         let cell:UITableViewCell = self.tableView.dequeueReusableCellWithIdentifier("cell")! as UITableViewCell
-        cell.textLabel?.text = self.listOfGroups[indexPath.row]
+        if self.segmentControl.selectedSegmentIndex == 0 {
+            cell.textLabel?.text = self.listOfGroups[indexPath.row]
+        }
+        else {
+            cell.textLabel?.text = self.listOfSelectedGroups[indexPath.row] as? String
+        }
         
         return cell
-        }
+    }
+    
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+     self.listOfSelectedGroups.addObject(self.listOfGroups[indexPath.row])
+    }
+    
+    @IBAction func indexChanged(sender: AnyObject) {
+        self.tableView.reloadData()
+    }
 }
+
